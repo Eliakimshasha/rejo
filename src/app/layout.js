8d8abd506a9a216448baa609ea6b68f03da66e66
eliakimshasha <eliakimshasha@gmail.com>
2025-06-20 00:28:44 +0300

    commited

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,11 +12,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {!path.startsWith("/payments") && <Header />}
-        <div className={` ${path.startsWith("/payments")? "mt-0":"mt-14"} bg-white `}>{children}</div>
-        {!path.startsWith("/routes") && !path.startsWith("/payments") && (
+        {!["authentication", "payments"].some((route) =>
+          path.startsWith(`/${route}`)
+        ) && <Header />}
+        <div
+          className={` ${
+            ["authentication", "payments"].some((route) =>
+          path.startsWith(`/${route}`)
+        ) ? "mt-0" : "mt-14"
+          } bg-white `}
+        >
+          {children}
+        </div>
+        {!["authentication", "payments", "routes"].some((route) =>
+          path.startsWith(`/${route}`)
+        ) &&
           <Footer />
-        )}
+        }
         {/* <Footer /> */}
       </body>
     </html>
